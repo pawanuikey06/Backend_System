a512995fe7a1c708a8a74c5d13c2c99f80709088
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-29 21:38:45 +0530

    Async Handler And Utils implementation

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ app.use(cors({
 
 }));
 
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public")) //public folder for storing assets
+app.use(cookieParser())
 
 
+ 
 
 export default app;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asynHandler =(requestHandeler)=>{
+     return (req,res,next)=>{
+        Promise.resolve((req,res,next))
+        .catch((err)=>next(err));
+     };
+
+};
+
+export default asynHandler;
+
+
+
+
+// other METHOD
+
+/*
+const asyncHandler =(fn)=>async(req,res,next)=>{
+    try{
+        await fn(req,res,next);
+
+    }catch(error){
+        res.status(error.code ||500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+*/
