f3f2705fa226df81e40be63ba520d91cc087e744
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-31 12:43:12 +0530

    token generation methods implemnted

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,4 +64,35 @@ userSchema.methods.isPasswordCorrect =async function (password) {
    return await bcrypt.compare(password,this.password)
     
 }
+
+userSchema.methods.generateAccessToken =function(){
+
+    return jwt.sign({
+        _id:this._id , //we can get from our database(MONGODB)
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    } ,//we are getting this payload from DB
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+)
+
+};
+userSchema.methods.generateRefreshToken =function(){
+    return jwt.sign({
+        _id:this._id , //we can get from our database(MONGODB)
+        
+    } ,//we are getting this payload from DB
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+
+)
+    
+};
+
 export const User=mongoose.model("User",userSchema);
